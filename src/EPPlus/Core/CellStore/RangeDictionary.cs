7daf094bc3be774baf716b6cba6c032ad15e9644
JanKallman <jan.kallman@epplussoftware.com>
2022-08-11 16:16:21 +0200

    WIP:Work on range dictionary

--- src/EPPlus/Core/CellStore/RangeDictionary.cs
+++ src/EPPlus/Core/CellStore/RangeDictionary.cs
@@ -6,20 +6,22 @@ using System.Collections.Generic;
 
 namespace OfficeOpenXml.Core.CellStore
 {
+    /// <summary>
+    /// This class stores ranges to keep track if they have been accessed before.
+    /// Merge will add the range and return any part not added before. 
+    /// </summary>
     internal class RangeDictionary
     {
-        //private class RangeSorter : IComparer<FormulaRangeAddress>
-        //{
-        //    public int Compare(FormulaRangeAddress x, FormulaRangeAddress y)
-        //    {
-        //        return (x.FromCol * ExcelPackage.MaxRows + x.FromRow) - (y.ToCol * ExcelPackage.MaxRows + y.ToRow);
-        //    }
-        //}
-        //RangeSorter _rangeSorter=new RangeSorter();
         internal Dictionary<int,List<long>> _addresses = new Dictionary<int, List<long>>();
-        internal bool Merge(FormulaRangeAddress newAddress)
+        /// <summary>
+        /// Merge the cell into the existing data and returns the ranges added.
+        /// </summary>
+        /// <param name="newAddress"></param>
+        /// <returns></returns>
+        internal bool Merge(ref FormulaRangeAddress newAddress)
         {
-            var isAdded = false;
+            var spillRanges= new List<long>();
+            byte isAdded = 0;
             for(int c=newAddress.FromCol;c<=newAddress.ToCol;c++)
             {
                 var rowSpan = ((newAddress.FromRow-1) << 20) | (newAddress.ToRow-1);
@@ -27,30 +29,135 @@ namespace OfficeOpenXml.Core.CellStore
                 {
                     rows = new List<long>();
                     rows.Add(rowSpan);
+                    spillRanges.Add(rowSpan);
                     _addresses.Add(c, rows);
+                    isAdded = 1;
                     continue;
                 }
                 var ix = rows.BinarySearch(rowSpan);
                 if(ix < 0)
                 {
-                    long spillRange;
                     ix = ~ix;
                     if (ix > 0) ix--;
-                    isAdded = VerifyAndAdd(newAddress, rowSpan, rows, ix, out spillRange);
-                    if(isAdded==false && ++ix<rows.Count)
-                    {
-                        isAdded = VerifyAndAdd(newAddress, rowSpan, rows, ix, out spillRange);
-                    }
+                    isAdded |= VerifyAndAdd(newAddress, rowSpan, rows, ix, spillRanges);
+                    //if (isAdded == false && ++ix < rows.Count)
+                    //{
+                    //    isAdded = VerifyAndAdd(newAddress, rowSpan, rows, ix, spillRanges);
+                    //}
+                    MergeWithNext(rows, ix);
                 }
             }
-            return isAdded;
+            if(isAdded == 1)
+            {
+                GetSpillRanges(spillRanges, ref newAddress);
+            }
+            return isAdded != 0;
         }
 
-        private static bool VerifyAndAdd(FormulaRangeAddress newAddress, int rowSpan, List<long> rows, int ix, out long spillRange)
+        private void GetSpillRanges(List<long> spillRanges, ref FormulaRangeAddress address)
+        {
+            int fromRow, toRow, fromCol, toCol;
+            fromRow = toRow = fromCol = toCol = -1;
+            var col = address.FromCol;
+            bool hasGap = false;
+            foreach(var r in spillRanges)
+            {
+                if(r<-1)
+                {
+                    return;
+                }
+                else
+                {
+                    var fr = (int)(r >> 20) + 1;
+                    var tr = (int)(r & 0xFFFFF) + 1;
+                    if (fromRow == -1)
+                    {
+                        fromRow = fr;
+                        toRow = tr;
+                        if (fromRow > 0)
+                        {
+                            fromCol = toCol = col;
+                        }
+                    }
+                    if(r==-1)
+                    {
+                        if(fromRow > 0)
+                        {
+                            hasGap = true;
+                        }
+                    }
+                    else 
+                    {
+                        if (fromRow == fr && toRow == tr && hasGap == false)
+                        {
+                            if (fromCol == 0) fromCol = col;
+                            toCol = col;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }                    
+                }
+                col++;
+            }
+            address.FromRow = fromRow;
+            address.ToRow = toRow;
+            address.FromCol = fromCol;
+            address.ToCol = toCol;
+        }
+
+        private static void MergeWithNext(List<long> rows, int ix)
+        {
+            do
+            {
+                if (ix + 1 >= rows.Count) break;
+                var fromRow1 = (int)(rows[ix] >> 20) + 1;
+                var toRow1 = (int)(rows[ix] & 0xFFFFF) + 1;
+                var fromRow2 = (int)(rows[ix + 1] >> 20) + 1;
+                var toRow2 = (int)(rows[ix + 1] & 0xFFFFF) + 1;
+                if (toRow1 + 1 >= fromRow2)
+                {
+                    rows[ix] = ((fromRow1 - 1) << 20) | (toRow2 - 1);
+                    rows.Remove(rows[ix + 1]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            while (true);
+        }
+
+        internal bool Exists(int row, int col)
+        {
+            if(_addresses.TryGetValue(col, out List<long> rows))
+            {
+                var rowSpan = ((row - 1) << 20) | (row - 1);
+                var ix = rows.BinarySearch(rowSpan);
+                if(ix<0)
+                {
+                    ix = ~ix;
+                    if (ix > 0) ix--;
+                    var fromRow = (int)(rows[ix] >> 20) + 1;
+                    var toRow = (int)(rows[ix] & 0xFFFFF) + 1;
+                    if(row >= fromRow && row <= toRow)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static byte VerifyAndAdd(FormulaRangeAddress newAddress, int rowSpan, List<long> rows, int ix, List<long> spillRanges)
         {
             var fromRow = (int)(rows[ix] >> 20) + 1;
             var toRow = (int)(rows[ix] & 0xFFFFF) + 1;
-            spillRange = 0;
             if (newAddress.FromRow > toRow)
             {
                 if(newAddress.FromRow-1==toRow) //Next to each other: Merge
@@ -61,7 +168,8 @@ namespace OfficeOpenXml.Core.CellStore
                 {
                     rows.Insert(ix + 1, rowSpan);
                 }
-                return true;
+                spillRanges.Add(rowSpan);
+                return 1;
             }
             else if (newAddress.ToRow < fromRow)
             {
@@ -73,32 +181,40 @@ namespace OfficeOpenXml.Core.CellStore
                 {
                     rows.Insert(ix, rowSpan);
                 }
-                return true;
+                spillRanges.Add(rowSpan);
+                return 1;
             }
             else
             {
                 if (newAddress.FromRow >= fromRow && newAddress.ToRow <= toRow) //Within, 
                 {
-                    return false;
+                    spillRanges.Add(-1);
+                    return 0;
                 }
                 else
                 {
+                    
                     if (newAddress.FromRow < fromRow && newAddress.ToRow <= toRow)
                     {
-                        spillRange = ((newAddress.FromRow - 1) << 20) | (fromRow - 1);
-                        rows[ix] = ((newAddress.FromRow) << 20) & (toRow - 1);
-                        //Set new Span
+                        spillRanges.Add(((newAddress.FromRow - 1) << 20) | (fromRow - 2));
+                        rows[ix] = ((newAddress.FromRow) << 20) | (toRow - 1);
                     }
                     if (newAddress.FromRow >= fromRow && newAddress.ToRow > toRow)
                     {
-                        spillRange = (toRow << 20) & (newAddress.ToRow - 1);
-                        rows[ix] = ((fromRow - 1) << 20) | (newAddress.ToRow - 1);
-                        //Set new Span
+                        if(newAddress.FromRow < fromRow && newAddress.ToRow <= toRow)
+                        {
+                            spillRanges[spillRanges.Count - 1] = -2;    //Partial address, return the full address at the end.
+                        }
+                        else
+                        {
+                            spillRanges.Add((toRow << 20) & (newAddress.ToRow - 1));
+                            rows[ix] = ((fromRow - 1) << 20) | (newAddress.ToRow - 1);
+                        }
                     }
                 }
             }
 
-            return false;
+            return 0;
         }
     }
 }
--- src/EPPlusTest/Core/RangeDictionaryTests.cs
+++ src/EPPlusTest/Core/RangeDictionaryTests.cs
@@ -18,21 +18,90 @@ namespace EPPlusTest.Core
         {
         }
         [TestMethod]
-        public void VerifyAddress()
+        public void VerifyMergeAddress1()
         {
             var rd = new RangeDictionary();
 
-            rd.Merge(new FormulaRangeAddress() { FromRow = 1, ToRow = 5, FromCol = 1, ToCol = 5 });
-            rd.Merge(new FormulaRangeAddress() { FromRow = 6, ToRow = 10, FromCol = 1, ToCol = 3 });
-            rd.Merge(new FormulaRangeAddress() { FromRow = 1, ToRow = 2, FromCol = 1, ToCol = 3 });
-            rd.Merge(new FormulaRangeAddress() { FromRow = 7, ToRow = 8, FromCol = 1, ToCol = 3 });
+            var r1 = new FormulaRangeAddress() { FromRow = 1, ToRow = 5, FromCol = 1, ToCol = 5 };
+            var b1=rd.Merge(ref r1);
+            var r2=new FormulaRangeAddress() { FromRow = 6, ToRow = 10, FromCol = 1, ToCol = 3 };
+            var b2 = rd.Merge(ref r2);
+            var r3 = new FormulaRangeAddress() { FromRow = 1, ToRow = 2, FromCol = 1, ToCol = 3 };
+            var b3 = rd.Merge(ref r3);
+            var r4 = new FormulaRangeAddress() { FromRow = 7, ToRow = 8, FromCol = 1, ToCol = 3 };
+            var b4 = rd.Merge(ref r4);
+            var r5 = new FormulaRangeAddress() { FromRow = 3, ToRow = 8, FromCol = 3, ToCol = 6 };
+            var b5 = rd.Merge(ref r5);
 
             Assert.AreEqual(9, rd._addresses[1][0]);
             Assert.AreEqual(9, rd._addresses[2][0]);
             Assert.AreEqual(9, rd._addresses[3][0]);
-            Assert.AreEqual(4, rd._addresses[4][0]);
-            Assert.AreEqual(4, rd._addresses[5][0]);
+            Assert.AreEqual(7, rd._addresses[4][0]);
+            Assert.AreEqual(7, rd._addresses[5][0]);
+            Assert.AreEqual(2097159, rd._addresses[6][0]);
 
+            Assert.IsTrue(rd.Exists(1, 1));
+            Assert.IsTrue(rd.Exists(6, 3));
+            Assert.IsTrue(rd.Exists(7, 3));
+            Assert.IsFalse(rd.Exists(2, 6));
+            Assert.IsTrue(rd.Exists(3, 6));
+            Assert.IsTrue(rd.Exists(8, 6));
+            Assert.IsFalse(rd.Exists(9, 6));
+        }
+        [TestMethod]
+        public void VerifyMergeAddressBetween()
+        {
+            var rd = new RangeDictionary();
+
+            var r1 = new FormulaRangeAddress() { FromRow = 2, ToRow = 5, FromCol = 1, ToCol = 5 };
+            var b1 = rd.Merge(ref r1);
+            var r2 = new FormulaRangeAddress() { FromRow = 7, ToRow = 10, FromCol = 1, ToCol = 3 };
+            var b2 = rd.Merge(ref r2);
+            var r3 = new FormulaRangeAddress() { FromRow = 6, ToRow = 6, FromCol = 1, ToCol = 3 };
+            var b3 = rd.Merge(ref r3);
+            var r4 = new FormulaRangeAddress() { FromRow = 1, ToRow = 1, FromCol = 1, ToCol = 10 };
+            var b4 = rd.Merge(ref r4);
+
+            Assert.IsTrue(rd.Exists(1, 1));
+        }
+        [TestMethod]
+        public void VerifyOrderIsSorted()
+        {
+            var rd = new RangeDictionary();
+
+            var r1 = new FormulaRangeAddress() { FromRow = 7, ToRow = 7, FromCol = 1, ToCol = 5 };
+            rd.Merge(ref r1);
+            var r2 = new FormulaRangeAddress() { FromRow = 5, ToRow = 5, FromCol = 1, ToCol = 5 };
+            rd.Merge(ref r2);
+            var r3 = new FormulaRangeAddress() { FromRow = 15, ToRow = 15, FromCol = 1, ToCol = 5 };
+            rd.Merge(ref r3);
+            var r4 = new FormulaRangeAddress() { FromRow = 9, ToRow = 9, FromCol = 1, ToCol = 5 };
+            rd.Merge(ref r4);
+            var r5 = new FormulaRangeAddress() { FromRow = 11, ToRow = 11, FromCol = 1, ToCol = 5 };
+            rd.Merge(ref r5);
+            var r6 = new FormulaRangeAddress() { FromRow = 1, ToRow = 2, FromCol = 1, ToCol = 5 };
+            rd.Merge(ref r6);
+            var r7 = new FormulaRangeAddress() { FromRow = 13, ToRow = 13, FromCol = 1, ToCol = 5 };
+            rd.Merge(ref r7);
+
+            Assert.AreEqual(1, (rd._addresses[1][0] >> 20) + 1);
+            Assert.AreEqual(5, (rd._addresses[1][1] >> 20) + 1);
+            Assert.AreEqual(7, (rd._addresses[1][2] >> 20) + 1);
+            Assert.AreEqual(9, (rd._addresses[1][3] >> 20) + 1);
+            Assert.AreEqual(11, (rd._addresses[1][4] >> 20) + 1);
+            Assert.AreEqual(13, (rd._addresses[1][5] >> 20) + 1);
+            Assert.AreEqual(15, (rd._addresses[1][6] >> 20) + 1);
+
+            Assert.IsTrue(rd.Exists(1, 1));
+        }
+        [TestMethod]
+        public void VerifySpillRanges1()
+        {
+            var rd = new RangeDictionary();
+            var r1 = new FormulaRangeAddress() { FromRow = 7, ToRow = 18, FromCol = 1, ToCol = 5 };
+            rd.Merge(ref r1);
+            var r2 = new FormulaRangeAddress() { FromRow = 5, ToRow = 18, FromCol = 1, ToCol = 5 };
+            var b2=rd.Merge(ref r2);
         }
     }
 }
